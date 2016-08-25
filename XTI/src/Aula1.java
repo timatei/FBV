ad912050845f08f0f1673e6c14031c71cb7e313a
Leandro <Leandro@SSM-Supervisor>
2016-08-25 10:57:46 -0300

    ok

---
+++ XTI/src/Aula1.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog(null, "Qual o Seu Nome?");
+		JOptionPane.showMessageDialog(null, nome);
+
+	}
+
+}
